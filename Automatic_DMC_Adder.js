688c86e07934171f8c9fad3e92c0daad9e379d7c
Jonathan Davey <Jonnyd456@hotmail.com>
2019-08-03 23:59:26 +0100

    Update Automatic_DMC_Adder.js

--- Automatic_DMC_Adder.js
+++ Automatic_DMC_Adder.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         PCStitch > Stitcher AutoShop
+// @name         Automatic DMC Adder
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  simple userscript that adds all your required threads from your PCStitch Export
